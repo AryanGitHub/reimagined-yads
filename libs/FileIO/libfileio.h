3b247074e2102aaa2752b6c997f6729c1ba75842
Aryan Pandey <aryanandrod26@gmail.com>
2022-02-26 09:34:18 +0530

    made createFileIfItDoesntExist from int to char* in libfileio.h

--- libs/FileIO/libfileio.h
+++ libs/FileIO/libfileio.h
@@ -5,6 +5,6 @@ int isFileExists (char* path);
 int isDirExists (char* path);
 char** getContents(char*** names, int* size,char *address);
 char* getPathToFile(char* name_of_file, char* path_to_directory);
-int createFileIfItDoesntExist(char* name_of_file , char* path_to_file);
+char* createFileIfItDoesntExist(char* name_of_file , char* path_to_file);
 char* createFolderIfItDoesntExist(char* name_of_folder, char* path_to_folder);
 #endif // FILEIO_H
